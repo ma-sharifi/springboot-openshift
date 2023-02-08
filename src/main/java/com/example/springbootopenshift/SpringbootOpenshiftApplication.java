3a147086e407c243b4738b537e1e9868a855cedc
Mahdi Sharifi <m.sharifi@bluerocktms.com>
2023-02-08 23:56:41 +0100

    first commit

---
+++ src/main/java/com/example/springbootopenshift/SpringbootOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOpenshiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootopenshift/SpringbootOpenshiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootopenshift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOpenshiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
